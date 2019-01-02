c227d662d0e989e362c61d9d16373ebabfa46588
苏征 <suzheng@souche.com>
2019-01-02 16:35:04 +0800

    短信验证码重构

--- security-core/src/main/java/su/security/core/authentication/mobile/SmsCodeAuthenticationSecurityConfig.java
+++ security-core/src/main/java/su/security/core/authentication/mobile/SmsCodeAuthenticationSecurityConfig.java
@@ -24,7 +24,7 @@ public class SmsCodeAuthenticationSecurityConfig extends SecurityConfigurerAdapt
     @Autowired
     private UserDetailsService userDetailsService;
     @Override
-    public void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http){
         SmsCodeAuthenticationFilter smsCodeAuthenticationFilter = new SmsCodeAuthenticationFilter();
         smsCodeAuthenticationFilter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class));
         smsCodeAuthenticationFilter.setAuthenticationSuccessHandler(suAuthenticationSuccessHandler);
